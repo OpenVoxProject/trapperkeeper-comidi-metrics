f5e8e22445ea0315c2a22a9f25804e9a2e4df46e
nmburgan <13688219+nmburgan@users.noreply.github.com>
2025-12-19 16:51:18 -0800

    Updates for OpenVox

--- project.clj
+++ project.clj
@@ -1,33 +1,33 @@
-(defproject puppetlabs/trapperkeeper-comidi-metrics "0.1.3-SNAPSHOT"
+(defproject org.openvoxproject/trapperkeeper-comidi-metrics "0.1.3-SNAPSHOT"
   :description "Comidi/HTTP Metrics for Trapperkeeper"
-  :url "http://github.com/puppetlabs/trapperkeeper-comidi-metrics"
+  :url "http://github.com/openvoxproject/trapperkeeper-comidi-metrics"
 
   :min-lein-version "2.9.1"
 
   :pedantic? :abort
 
-  :parent-project {:coords [puppetlabs/clj-parent "4.6.14"]
+  :parent-project {:coords [org.openvoxproject/clj-parent "7.4.1-SNAPSHOT"]
                    :inherit [:managed-dependencies]}
 
   :dependencies [[org.clojure/clojure]
                  [prismatic/schema]
-                 [puppetlabs/trapperkeeper-metrics]
-                 [puppetlabs/comidi]]
+                 [org.openvoxproject/trapperkeeper-metrics]
+                 [org.openvoxproject/comidi]]
 
   :plugins [[lein-parent "0.3.7"]]
 
-  :deploy-repositories [["releases" {:url "https://clojars.org/repo"
-                                     :username :env/clojars_jenkins_username
-                                     :password :env/clojars_jenkins_password
+  :deploy-repositories [["clojars" {:url "https://clojars.org/repo"
+                                     :username :env/CLOJARS_USERNAME
+                                     :password :env/CLOJARS_PASSWORD
                                      :sign-releases false}]]
 
   :profiles {:dev {:source-paths ["dev"]
-                   :dependencies [[puppetlabs/trapperkeeper :classifier "test"]
-                                  [puppetlabs/kitchensink :classifier "test"]
-                                  [puppetlabs/trapperkeeper-status]
-                                  [puppetlabs/http-client]
+                   :dependencies [[org.openvoxproject/trapperkeeper :classifier "test"]
+                                  [org.openvoxproject/kitchensink :classifier "test"]
+                                  [org.openvoxproject/trapperkeeper-status]
+                                  [org.openvoxproject/http-client]
                                   [org.bouncycastle/bcpkix-jdk15on]
-                                  [puppetlabs/trapperkeeper-webserver-jetty9]]}}
+                                  [org.openvoxproject/trapperkeeper-webserver-jetty9]]}}
 
   :aliases {"example" ["run" "-m" "example.comidi-metrics-web-app"]
             "example-data" ["run" "-m" "example.traffic-generator"]})
