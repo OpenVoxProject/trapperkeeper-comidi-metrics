bbb36374127b480f5d5d31a57446e920e0166e5a
OpenVoxProjectBot <215568489+OpenVoxProjectBot@users.noreply.github.com>
2025-12-31 23:20:23 +0000

    Version 0.1.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.openvoxproject/trapperkeeper-comidi-metrics "0.1.3-SNAPSHOT"
+(defproject org.openvoxproject/trapperkeeper-comidi-metrics "0.1.3"
   :description "Comidi/HTTP Metrics for Trapperkeeper"
   :url "http://github.com/openvoxproject/trapperkeeper-comidi-metrics"
   :license {:name "Apache-2.0"
